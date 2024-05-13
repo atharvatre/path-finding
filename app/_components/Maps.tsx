e912e247b5071a3ce215096188c2be0fbc70c6ca
atharvatre <91801614+atharvatre@users.noreply.github.com>
2024-05-14 02:19:41 +0530

    map styling

--- app/_components/Maps.tsx
+++ app/_components/Maps.tsx
@@ -1,9 +1,11 @@
 "use client";
-
 import React from "react";
 import { useState, useEffect } from "react";
-import mapboxgl, { accessToken } from "mapbox-gl";
+import mapboxgl from "mapbox-gl";
+// import "mapbox-gl/dist/mapbox-gl.css";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+
+// import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 const MAPBOX_ACCESS_TOKEN =
@@ -14,75 +16,54 @@ interface MapProps {
 }
 
 const Maps: React.FC<MapProps> = ({ containerId }) => {
-  const [placingMarker, setPlacingMarker] =
-    useState<boolean>(false);
-
-  const [originMarker, setOriginMarker] =
-    useState<mapboxgl.Marker | null>(null);
-  const [destinationMarker, setDestinationMarker] =
-    useState<mapboxgl.Marker | null>(null);
-
   useEffect(() => {
     mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
     const map = new mapboxgl.Map({
       container: containerId,
-      style: "mapbox://styles/mapbox/streets-v11",
-      //center: [0, 0],
-      zoom: 9,
+      style:
+        "mapbox://styles/darthcoder/clw5af2py017q01pf6fwl1cn8",
+      center: [21.842984855770126, 76.3504098261338],
+      zoom: 5,
+      minZoom: 3,
+      maxZoom: 22,
     });
 
     //geocoder control
     const geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl,
+      placeholder: "search for a location",
     });
     map.addControl(geocoder);
 
-    geocoder.on("result", (e: any) => {
-      map.flyTo({ center: e.result.center });
-    });
+    map.addControl(
+      new mapboxgl.NavigationControl(),
+      "top-right"
+    );
 
-    map.on("click", (e: any) => {
-      if (placingMarker) {
-        console.log("toggleon");
-        if (!originMarker) {
-          const marker = new mapboxgl.Marker({
-            color: "green",
-          })
-            .setLngLat(e.lngLat)
-            .addTo(map);
-          setOriginMarker(marker);
-        } else if (!destinationMarker) {
-          const marker = new mapboxgl.Marker({
-            color: "red",
-          })
-            .setLngLat(e.lngLat)
-            .addTo(map);
-          setDestinationMarker(marker);
-        } else {
-          originMarker.remove();
-          destinationMarker.remove();
-          setOriginMarker(null);
-          setDestinationMarker(null);
-        }
-      }
+    geocoder.on("result", (e: any) => {
+      map.flyTo({ center: e.result.center, zoom: 10 });
     });
 
     return () => map.remove();
-  }, [containerId, originMarker, destinationMarker]);
+  }, [containerId]);
   return (
-    <div className="">
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
       <div
         id={containerId}
-        className="h-2/3 w-2/3 absolute right-4 left-4 mt-14"
-      ></div>
-
-      <button
-        className="bg-blue-700 text-white rounded-md p-3 "
-        onClick={() => setPlacingMarker(!placingMarker)}
-      >
-        Toggle marker
-      </button>
+        style={{
+          width: "80vw",
+          height: "80vh",
+          position: "relative",
+        }}
+      />
     </div>
   );
 };
---
+++ app/_mapbox/Map.tsx
@@ -0,0 +1,16 @@
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect } from "react";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGFydGhjb2RlciIsImEiOiJjbHZ4ZWtpcTUxeGk2Mmlud2NjdDlhNzZtIn0.E8le2WKpLYoYBByGVrUXpQ";
+
+const Map = () => {
+  useEffect();
+  return;
+  <>
+    <div className=""></div>
+  </>;
+};
+
+export default Map;
